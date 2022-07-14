11b62c9dddc87e660999f97205a1690a32ec025e
bpeixoto96 <peixotobruno17@gmail.com>
2022-07-14 17:04:37 -0300

    Csharp IMC calculator

---
+++ Project IMC Csharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+float A, P, imc;
+
+Console.Write("Digite o seu peso:");
+P = float.Parse(Console.ReadLine());
+
+
+Console.Write("Digite o seu altura:");
+A = float.Parse(Console.ReadLine());
+
+imc = (P / (A * A));
+
+if (imc <= 18.5)
+{
+    Console.WriteLine("Peso abaixo do normal");
+}
+else if ((imc > 18.5) && (imc < 25))
+{
+    Console.WriteLine("Peso normal");
+}
+else if ((imc >= 25) && (imc < 30))
+{
+    Console.WriteLine("Sobre o Peso");
+}
+else if ((imc >= 30) && (imc < 35))
+{
+    Console.WriteLine("Grau de Obesidade I");
+}
+else if ((imc >= 35) && (imc < 40))
+{
+    Console.WriteLine("Grau de Obesidade II");
+}
+else if (imc >= 40)
+{
+    Console.WriteLine("Obesidade Grau III");
+}
+
